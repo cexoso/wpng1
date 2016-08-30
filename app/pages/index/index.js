ed0c97a6009d0bfc07a8c076411ffe570b379b29
熊杰 <xiongj01@mySoft.com.cn>
2016-08-30 22:02:40 +0800

    add file loader

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,6 +1,8 @@
 import angular from "angular";
 import "./index.scss";
+import a from "resource/img/e.jpg";
 
 angular.module('app').controller("indexCtrl",["$scope",($scope)=>{
     $scope.placeHolder = "anna";
+	console.log(a)
 }]);
--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,34 @@ var webpack = require("webpack");
 var path = require("path");
 var rootDir = __dirname;
 module.exports = {
-        entry: {
-            app: ["./app/index.js"]
-        },
-        output: {
-            path: path.resolve(__dirname, "build"),
-            filename: "bundle.js"
-        },
-		devtool: "source-map",
-        module: {
-            loaders: [{
-                loaders: ['babel'],
-                test: /\.(js)$/,
-                exclude: /node_modules/
-            }, {
-                loaders: ["style", "css?sourceMap", "sass?sourceMap"],
-                test: /\.scss$/
-            }, {
-                test: /\.jpe?g$|\.gif$|\.png$|\.ico$|\.svg$|\.woff$|\.ttf$|\.eot$/,
-                loader: "file-loader?name=[path][name]-[hash:8].[ext]&context=" + rootDir
-            }]
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin()
-        ]
+    entry: {
+        app: ["./app/index.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    devtool: "source-map",
+    module: {
+        loaders: [{
+            loaders: ['babel'],
+            test: /\.(js)$/,
+            exclude: /node_modules/
+        }, {
+            loaders: ["style", "css?sourceMap", "sass?sourceMap"],
+            test: /\.scss$/
+        }, {
+            test: /\.jpe?g$|\.gif$|\.png$|\.ico$|\.svg$|\.woff$|\.ttf$|\.eot$/,
+            loader: "file-loader?name=[path][name]-[hash:8].[ext]&context=" + rootDir
+        }]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+	resolve: {
+        alias: {
+			"resource": path.join(__dirname, "./app/resource")
+        }
     }
-    /*eslint-enable */
+}
+/*eslint-enable */
