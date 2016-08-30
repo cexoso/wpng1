4af83dc75d49d8a2d9d6f90171936fdca9384996
熊杰 <xiongj01@mySoft.com.cn>
2016-08-30 22:29:46 +0800

    add opn open the url when run dev

--- bin/devServer.js
+++ bin/devServer.js
@@ -1,6 +1,7 @@
 /*eslint-disable */
 var webpack = require("webpack");
 var config = require("../webpack.config.js");
+var opn = require("opn");
 var WebpackDevServer = require("webpack-dev-server");
 config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080", "webpack/hot/dev-server");
 var compiler = webpack(config);
@@ -9,4 +10,6 @@ var server = new WebpackDevServer(compiler,{
     contentBase: "./app"
 });
 server.listen(8080);
+opn("http://localhost:8080");
+
 /*eslint-enable */
