95f756c2964e8653a4a088fbed4a5b637950ce4e
徐媛 <xuy04@mysoft.com.cn>
2016-09-02 16:53:15 +0800

    new char

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -4,7 +4,7 @@ import a from "resource/img/e.jpg";
 import aa from "../../resource/img/e.jpg";
 
 angular.module('app').controller("indexCtrl",["$scope",($scope)=>{
-    $scope.placeHolder = "anna and jack";
+    $scope.placeHolder = "anna and jack1";
 	console.log(a)
 	console.log(aa)
 }]);
