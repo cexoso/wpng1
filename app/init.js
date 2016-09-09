a7f5438528271c466430d9ab552eef742424f0ab
徐媛 <xuy04@mysoft.com.cn>
2016-09-09 14:28:10 +0800

    *

--- app/init.js
+++ app/init.js
@@ -22,7 +22,6 @@
 *************************************************/
 import angular from "angular";
 import "resource/bootstrap/css/bootstrap.min.css";
-import "resource/bootstrap/css/bootstrap-theme.min.css";
 import "resource/css/base.less";
 import "resource/css/top_footer.less";
 import "resource/css/nav.less";
