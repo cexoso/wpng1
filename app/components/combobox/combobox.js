053024fbd6eca5f882358aeb020237ef3334a1cb
熊杰 <xiongj01@mySoft.com.cn>
2016-09-13 12:59:47 +0800

    初步combox组件

--- app/components/combobox/combobox.js
+++ app/components/combobox/combobox.js
@@ -1,10 +1,24 @@
 import angular from "angular";
+import tpl from "html!./combobox.html";
 export default function (components) {
     angular.module(components).directive('ceCombobox',[()=>{
         return {
-            link() {
-                console.log("link")
-            }
+			template: tpl,
+			scope: {
+				data: "=",
+				ngModel: "="
+			},
+			controller: ["$scope",($scope)=>{
+				$scope.showCombobox = false;
+				$scope.comboxClickHandle = function () {
+					$scope.showCombobox = !$scope.showCombobox;
+				}
+				$scope.onChose = function (d) {
+					console.log(d)
+					$scope.showCombobox = false;
+					$scope.ngModel = d;
+				}
+			}]
         }
     }]);
 }
--- app/pages/ws/wsCtrl.js
+++ app/pages/ws/wsCtrl.js
@@ -1,4 +1,32 @@
 import angular from "angular";
 angular.module('app').controller("wsCtrl", ["$scope", ($scope) => {
-    console.log($scope)
+	$scope.data1 = [{
+		name: 1,
+		value: 1
+	},{
+		name: 2,
+		value: 2
+	},{
+		name: 3,
+		value: 3
+	},{
+		name: 4,
+		value: 4
+	},{
+		name: 5,
+		value: 5
+	}]
+	$scope.data2 = [{
+		name: 1,
+		value: 1
+	},{
+		name: 2,
+		value: 2
+	},{
+		name: 3,
+		value: 3
+	},{
+		name: 4,
+		value: 4
+	}]
 }])
