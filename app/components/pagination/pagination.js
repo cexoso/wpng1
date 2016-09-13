596c490d3f58dec5db733abe151fded6af86f366
cexoso <285516193@qq.com>
2016-09-13 23:17:53 +0800

    分页

--- app/components/pagination/pagination.js
+++ app/components/pagination/pagination.js
@@ -1,5 +1,18 @@
 import angular from "angular";
 import tpl from "html!./pagination.html";
+function range(from,to) {
+    const arr = [];
+    for (let i = from; i <= to; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+function min(a,b) {
+    return a < b ? a : b;
+}
+function max(a,b) {
+    return a > b ? a : b;
+}
 export default function (components) {
     angular.module(components).directive('cePagination',[()=>{
         return {
@@ -15,6 +28,10 @@ export default function (components) {
 					if (currentPage === void 0 || total === void 0) {
 						return void 0;
 					}
+                    const from = max(currentPage - 3,1);
+                    const to = min(currentPage + 3,total);
+                    const pages = range(from,to);
+                    $scope.pages = pages;
 				}
 				$scope.$watch("currentPage",changePages)
 				$scope.$watch("total",changePages)
