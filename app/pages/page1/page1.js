3d3de85d1358160182d968af6e442cee313bb869
熊杰 <xiongj01@mySoft.com.cn>
2016-09-19 10:19:54 +0800

    modal可以了

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -1,5 +1,13 @@
 import angular from "angular";
 angular.module('app').controller("AccordionDemoCtrl", ["$scope", "modal",($scope,modal) => {
+	modal.open({
+		controller: "modalCtrl",
+		template: "<h1>{{q}}</h1>"
+	});
+	modal.open({
+		controller: "modalCtrl",
+		template: "<h1>{{q}}</h1>"
+	});
 	$scope.open = function () {
 		modal.open({
 			controller: "modalCtrl",
@@ -10,5 +18,5 @@ angular.module('app').controller("AccordionDemoCtrl", ["$scope", "modal",($scope
 	$scope.q = 1;
 	setTimeout(()=>{
 		modalInstance.close();
-	},100000);
+	},1000);
 }]);
