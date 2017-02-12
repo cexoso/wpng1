1f739f9275a512ff33975a81d26c121d341e5a8b
cexoso <285516193@qq.com>
2017-02-12 15:27:28 +0800

    原来 ng-controller也是可以支持作用域内定义 的构造函数的。

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -2,73 +2,93 @@ import angular from "angular";
 import "./css/index.less";
 import _ from "lodash";
 const tpl = require("html!./tpl.html");
-angular.module('app').controller("indexCtrl",["$scope",(s)=>{
-    s.data = {
-		name: "root",
-		org: [{
-			name: "org1",
+angular.module('app')
+	.controller("indexCtrl", ["$scope", (s) => {
+		s.data = {
+			name: "root",
 			org: [{
-				name: "org11"
+				name: "org1",
+				org: [{
+					name: "org11"
+				}],
+				proj: [{
+					name: "proj11"
+				}, {
+					name: "root",
+					org: [{
+						name: "org1",
+						org: [{
+							name: "org11"
+						}],
+						proj: [{
+							name: "proj11"
+						}]
+					}],
+					proj: [{
+						name: "proj1"
+					}]
+				}]
 			}],
 			proj: [{
-				name: "proj11"
+				name: "proj1"
 			}]
-		}],
-		proj: [{
-			name: "proj1"
-		}]
-	}
-	s.tpl = tpl
-}])
-.directive("include",["$compile",($compile)=>{
-	return {
-		restrict: "E",
-		link(s,e,a) {
-			s.$watch(scope=>scope.$eval(a.tpl),(n)=>{
-				if (n !== void 0) {
-					e.html(n);
-					$compile(e.contents())(s);
+		}
+		s.tpl = tpl
+		s.nodeCtrl = ["$scope", (s) => {
+			const {isEmpty} = _;
+			s.data.isClose = s.level === 0 ? 2 : 0;
+			s.hasChilren = function (d) {
+				return !isEmpty(d.org) || !isEmpty(d.proj);
+			}
+			s.$watch("data.selected", (n) => {
+				const {father} = s;
+				if (father && n !== void 0) {
+					const {org, proj} = father;
+					const children = _.compact(_.concat(org, proj));
+					const groupBy = _.groupBy(children, c => c.selected === void 0 ? 0 : c.selected);
+					father.selected = _.keys(groupBy).length > 1 ? 1 : n;
 				}
-			})
-		}
-	}
-}])
-.controller("nodeCtrl",["$scope",(s)=>{
-	const {isEmpty} = _;
-	s.hasChilren = function (d) {
-		return !isEmpty(d.org) || !isEmpty(d.proj);
-	}
-	s.$watch("data.selected",(n)=>{
-		const {father} = s;
-		if (father && n !== void 0) {
-			const {org,proj} = father;
-			const children = _.compact(_.concat(org,proj));
-			const groupBy = _.groupBy(children,c=>c.selected === void 0 ? 0 : c.selected);
-			father.selected = _.keys(groupBy).length > 1 ? 1 : n;
-		}
-	});
-	s.change = function (data,xx) {
-		_.map(data.org,o=>o.selected = xx);
-		_.map(data.proj,p=>p.selected = xx);
-	}
-}])
-.directive("inputM",[()=>{
-	return {
-		restrict: "E",
-		template: `<span ng-click="clickHandle()">{{format(model)}}</span>`,
-		scope: {
-			model: "=",
-			change: "&"
-		},
-		controller: ["$scope",(s)=>{
-			s.clickHandle = function () {
-				s.model = s.model ? 0 : 2;
-				setTimeout(()=>s.$apply(s.change),0);
+			});
+			function set(data, xx) {
+				data.selected = xx;
+				_.map(data.org, o => set(o, xx));
+				_.map(data.proj, o => set(o, xx));
 			}
-			s.format = function (model) {
-				return ["空","半","选"][model || 0];
+			s.change = function (data, xx) {
+				set(data, xx);
 			}
 		}]
-	}
-}])
+	}])
+	.directive("include", ["$compile", ($compile) => {
+		return {
+			restrict: "E",
+			link(s, e, a) {
+				s.$watch(scope => scope.$eval(a.tpl), (n) => {
+					if (n !== void 0) {
+						e.html(n);
+						$compile(e.contents())(s);
+					}
+				})
+			}
+		}
+	}])
+	.directive("inputM", [() => {
+		return {
+			restrict: "E",
+			template: `<span ng-click="clickHandle()">{{format(model)}}</span>`,
+			scope: {
+				model: "=",
+				change: "&"
+			},
+			controller: ["$scope", (s) => {
+				s.clickHandle = function () {
+					s.model = s.model ? 0 : 2;
+					setTimeout(() => s.$apply(s.change), 0);
+				}
+				s.format = function (model) {
+					return ["空", "半", "选"][model || 0];
+				}
+			}]
+		}
+	}])
 
