e3d51e9dbcf1112b16f781bf22fd663c756af221
cexoso <285516193@qq.com>
2017-02-12 15:54:48 +0800

    提到外面去

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -2,6 +2,30 @@ import angular from "angular";
 import "./css/index.less";
 import _ from "lodash";
 const tpl = require("html!./tpl.html");
+const nodeCtrl = ["$scope", (s) => {
+	const {isEmpty} = _;
+	s.data.isClose = s.level === 0 ? 2 : 0;
+	s.hasChilren = function (d) {
+		return !isEmpty(d.org) || !isEmpty(d.proj);
+	}
+	s.$watch("data.selected", (n) => {
+		const {father} = s;
+		if (father && n !== void 0) {
+			const {org, proj} = father;
+			const children = _.compact(_.concat(org, proj));
+			const groupBy = _.groupBy(children, c => c.selected === void 0 ? 0 : c.selected);
+			father.selected = _.keys(groupBy).length > 1 ? 1 : n;
+		}
+	});
+	function set(data, xx) {
+		data.selected = xx;
+		_.map(data.org, o => set(o, xx));
+		_.map(data.proj, o => set(o, xx));
+	}
+	s.change = function (data, xx) {
+		set(data, xx);
+	}
+}]
 angular.module('app')
 	.controller("indexCtrl", ["$scope", (s) => {
 		s.data = {
@@ -34,30 +58,7 @@ angular.module('app')
 			}]
 		}
 		s.tpl = tpl
-		s.nodeCtrl = ["$scope", (s) => {
-			const {isEmpty} = _;
-			s.data.isClose = s.level === 0 ? 2 : 0;
-			s.hasChilren = function (d) {
-				return !isEmpty(d.org) || !isEmpty(d.proj);
-			}
-			s.$watch("data.selected", (n) => {
-				const {father} = s;
-				if (father && n !== void 0) {
-					const {org, proj} = father;
-					const children = _.compact(_.concat(org, proj));
-					const groupBy = _.groupBy(children, c => c.selected === void 0 ? 0 : c.selected);
-					father.selected = _.keys(groupBy).length > 1 ? 1 : n;
-				}
-			});
-			function set(data, xx) {
-				data.selected = xx;
-				_.map(data.org, o => set(o, xx));
-				_.map(data.proj, o => set(o, xx));
-			}
-			s.change = function (data, xx) {
-				set(data, xx);
-			}
-		}]
+		s.nodeCtrl = nodeCtrl;
 	}])
 	.directive("include", ["$compile", ($compile) => {
 		return {
